f20445860633b5fdb567f92769e60184c72a733a
21salazarf <fa.salazar@uniandes.edu.co>
2025-09-15 22:49:41 -0500

    2

--- App/logic.py
+++ App/logic.py
@@ -1,11 +1,20 @@
+from datetime import datetime
 import time
+import csv
+import os
+from DataStructures import array_list as al
+from DataStructures import single_linked_list as sll
+
+csv.field_size_limit(2147483647)
+data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
 
 def new_logic():
-    """
-    Crea el catalogo para almacenar las estructuras de datos
-    """
-    #TODO: Llama a las funciónes de creación de las estructuras de datos
-    pass
+    
+    catalog = {
+        "taxis" : al.new_list(),
+        "neighborhoods" : al.new_list(),
+    }
+    return catalog
 
 
 # Funciones para la carga de datos
@@ -14,18 +23,109 @@ def load_data(catalog, filename):
     """
     Carga los datos del reto
     """
-    # TODO: Realizar la carga de datos
-    pass
+    inicio = get_time()   
+    
+    n_archivo = "Data/" + filename
+
+    archivo = csv.DictReader(open(n_archivo, encoding='utf-8'))    
+
+    for llave in archivo:
+
+        llave["pickup_datetime"] = datetime.datetime.strptime(llave["pickup_datetime"], "%Y-%m-%d %H:%M:%S")
+        llave["dropoff_datetime"] = datetime.datetime.strptime(llave["dropoff_datetime"], "%Y-%m-%d %H:%M:%S")
+        llave["passenger_count"] = int(llave["passenger_count"])
+        llave["trip_distance"] = float(llave["trip_distance"])
+        llave["pickup_longitude"] = float(llave["pickup_longitude"])
+        llave["pickup_latitude"] = float(llave["pickup_latitude"])
+        llave["rate_code"] = int(llave["rate_code"])
+        llave["dropoff_longitude"] = float(llave["dropoff_longitude"])
+        llave["dropoff_latitude"] = float(llave["dropoff_latitude"])
+        llave["payment_type"] = str(llave["payment_type"])
+        llave["fare_amount"] = float(llave["fare_amount"])
+        llave["extra"] = float(llave["extra"])
+        llave["mta_tax"] = float(llave["mta_tax"])
+        llave["tip_amount"] = float(llave["tip_amount"])
+        llave["tolls_amount"] = float(llave["tolls_amount"])
+        llave["improvement_surcharge"] = float(llave["improvement_surcharge"])
+        llave["total_amount"] = float(llave["total_amount"])
+        
+        al.add_last(catalog["taxis"], llave)
+        
+    filename2 = "Data/nyc-neighborhoods.csv"
+    
+    archivo2 = csv.DictReader(open(filename2, encoding='utf-8'))
+
+    for llave2 in archivo2:
+        llave2["borough"] = str(llave2["borough"])
+        llave2["neighborhood"] = str(llave2["neighborhood"])
+        llave2["latitude"] = float(llave2["latitude"])
+        llave2["longitude"] = float(llave2["longitude"])
+
+        al.add_last(catalog["neighborhoods"], llave2)
+
+    tamanio = catalog["taxis"]["size"]
+    
+    i = 0
+    
+    min = al.get_element(catalog["taxis"], 0)
+
+    max = min
+
+    while min["trip_distance"] <= 0.0:
+        i +=1
+        min = al.get_element(catalog["taxis"], i)
+
+    for valor in range(0, catalog["taxis"]["size"]):
+        elem = al.get_element(catalog["taxis"], valor)
+        if elem["trip_distance"] < min["trip_distance"] and elem["trip_distance"] > 0.0:
+            min = elem
+        elif elem["trip_distance"] > max["trip_distance"]:
+            max = elem
+
+    mas_corto = (min["pickup_datetime"], min["trip_distance"], min["total_amount"])
+
+    mas_largo = (max["pickup_datetime"], max["trip_distance"], max["total_amount"])
+    
+    primeros = []
+    
+    ultimos = []
+    
+    for i in range(0, 5):
+
+        element = al.get_element(catalog["taxis"], i)
+
+        resta = element["dropoff_datetime"] - element["pickup_datetime"]
+        minutos = resta.total_seconds()/60
+
+        info = [element["pickup_datetime"], element["dropoff_datetime"], minutos, element["trip_distance"], element["total_amount"]]
+        primeros.append(info)
+
+    for i in range(tamanio-5, tamanio):
+        elemento = al.get_element(catalog["taxis"], i)
+        
+        resta = elemento["dropoff_datetime"] - elemento["pickup_datetime"]
+        minutos = resta.total_seconds()/60
+        
+        info = [elemento["pickup_datetime"], elemento["dropoff_datetime"], minutos, elemento["trip_distance"], elemento["total_amount"]]
+        ultimos.append(info)
+
+    
+    final = get_time()
+    tiempo = delta_time(inicio, final)
+    
+    retorno = catalog, tiempo, tamanio, mas_corto, mas_largo, primeros, ultimos
+    
+    return retorno
+
 
 # Funciones de consulta sobre el catálogo
 
 def get_data(catalog, id):
-    """
-    Retorna un dato por su ID.
-    """
-    #TODO: Consulta en las Llamar la función del modelo para obtener un dato
-    pass
-
+    
+    size = catalog["taxis"]["size"]
+    if size > 0 and id < size:
+        element = al.get_element(catalog["taxis"], id)
+        return element
 
 def req_1(catalog):
     """
